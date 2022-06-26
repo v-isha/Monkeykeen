94d82f81d41079ba86bb2eba43e053d78858acb5
isha <vshlkmr81@gmail.com>
2022-06-26 16:53:32 +0530

    address flow

--- app/urls.py
+++ app/urls.py
@@ -16,23 +16,24 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='app/changepassword.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone/'), name='changepassword'),
     path('passwordchangedone/', auth_views.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
-    path('profile/', views.profile, name='profile'),
+    
+
     path("password-reset/", auth_views.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name="password_reset"),
     path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'), name="password_reset_done"),
     path("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html', form_class=MySetPasswordForm), name="password_reset_confirm"),
     path("password-reset-complete/", auth_views.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name="password_reset_complete"),
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    path('profile/', views.ProfileView.as_view(), name='profile'),
     path('address/', views.address, name='address'),
+    
+    
+    
+    
+    
+    
+    
+    
+    
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('orders/', views.orders, name='orders'),
--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
+from django import views
 from django.shortcuts import render
 from . models import Product
-from .forms import CustomerRegistrationForm ,LoginForm
+from .forms import CustomerRegistrationForm ,LoginForm , CustomerProfileForm
 from django.views import View
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate,login,logout
-from .models import User
+from .models import User ,  Customer
 
 
 #Landing page 
@@ -52,21 +53,55 @@ def add_to_cart(request):
 def buy_now(request):
  return render(request, 'app/buynow.html')
 
-def profile(request):
- return render(request, 'app/profile.html')
 
-def address(request):
- return render(request, 'app/address.html')
+
 
 def orders(request):
  return render(request, 'app/orders.html')
 
+def checkout(request):
+ return render(request, 'app/checkout.html')
+
+class ProfileView(View):
+	def get(self, request):
+		# totalitem = 0
+		# if request.user.is_authenticated:
+		# 	totalitem = len(Cart.objects.filter(user=request.user))
+		form = CustomerProfileForm()
+		return render(request, 'app/profile.html',{'form':form, 'active':'btn-primary'})
+		# return render(request, 'app/profile.html', {'form':form, 'active':'btn-primary', 'totalitem':totalitem})
+		
+	def post(self, request):
+		# totalitem = 0
+		# if request.user.is_authenticated:
+		# 	totalitem = len(Cart.objects.filter(user=request.user))
+		form = CustomerProfileForm(request.POST)
+		if form.is_valid():
+			usr = request.user
+			name  = form.cleaned_data['name']
+			locality = form.cleaned_data['locality']
+			city = form.cleaned_data['city']
+			state = form.cleaned_data['state']
+			zipcode = form.cleaned_data['zipcode']
+			reg = Customer(user=usr, name=name, locality=locality, city=city, state=state, zipcode=zipcode)
+			reg.save()
+			messages.success(request, 'Congratulations!! Profile Updated Successfully.')
+      
+		return render(request, 'app/profile.html', {'form':form, 'active':'btn-primary',})
+		# return render(request, 'app/profile.html', {'form':form, 'active':'btn-primary', 'totalitem':totalitem})
 
 
 
 
-# def checkout(request):
-#  return render(request, 'app/checkout.html')
+def address(request):
+  add = Customer.objects.filter(user=request.user)
+  return render(request, 'app/address.html',{"add":add,"active":"btn-primary"})
+
+
+
+
+
+
 
 
 
